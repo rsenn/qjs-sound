01151b9f9b6c8a7a39a522f5dc6bab4ceed7edcc
Roman Senn <roman.l.senn@gmail.com>
2025-04-13 08:19:07 +0200

    ...

--- test-labsound.cpp
+++ test-labsound.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <thread>
+#include <chrono>
 
 #include "test-labsound.hpp"
 #include "LabSound/core/GainNode.h"
@@ -10,19 +11,20 @@ using lab::OscillatorType;
 
 int
 main(int argc, char* argv[]) {
-  AudioStreamConfig _inputConfig;
-  AudioStreamConfig _outputConfig;
   auto config = GetDefaultAudioDeviceConfiguration(true);
-  _inputConfig = config.first;
-  _outputConfig = config.second;
-  std::shared_ptr<lab::AudioDevice_RtAudio> device(
-      new lab::AudioDevice_RtAudio(_inputConfig, _outputConfig));
+ AudioStreamConfig _inputConfig = config.first,  _outputConfig = config.second;
+
+  std::shared_ptr<lab::AudioDevice_RtAudio> device(new lab::AudioDevice_RtAudio(_inputConfig, _outputConfig));
+
   auto context = std::make_shared<lab::AudioContext>(false, true);
+  
   auto destinationNode = std::make_shared<lab::AudioDestinationNode>(*context.get(), device);
+  
   device->setDestinationNode(destinationNode);
   context->setDestinationNode(destinationNode);
 
   auto oscillator = std::make_shared<OscillatorNode>(*context.get());
+
   auto gain = std::make_shared<GainNode>(*context.get());
   gain->gain()->setValue(0.0625f);
 
@@ -33,6 +35,8 @@ main(int argc, char* argv[]) {
   oscillator->frequency()->setValue(440.f);
   oscillator->setType(OscillatorType::SINE);
   oscillator->start(0.0f);
+  
   std::this_thread::sleep_for(std::chrono::seconds(1));
+  
   return 0;
 }
