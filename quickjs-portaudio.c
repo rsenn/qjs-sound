df87c3ac4d206eb090beb9f7f935ea153f51bce4
Roman Senn <roman.l.senn@gmail.com>
2026-02-15 01:12:00 +0100

    ...

--- quickjs-portaudio.c
+++ quickjs-portaudio.c
@@ -9,7 +9,7 @@ static JSValue pastream_proto, pastream_ctor, audiodestinationnode_proto, audiod
     audiolistener_proto, audiolistener_ctor, audiodevice_proto, audiodevice_ctor;
 
 static JSValue
-js_pastream_constructor(JSStream* ctx, JSValueConst new_target, int argc, JSValueConst argv[]) {
+js_pastream_constructor(JSContext* ctx, JSValueConst new_target, int argc, JSValueConst argv[]) {
   JSValue proto, obj = JS_UNDEFINED;
 
   PaStream* st = js_mallocz(ctx, sizeof(PaStream));
@@ -39,14 +39,17 @@ fail:
   return JS_EXCEPTION;
 }
 
-enum {};
+enum {
+
+  PROP_INDEX,
+};
 
 static JSValue
-js_pastream_get(JSStream* ctx, JSValueConst this_val, int magic) {
-  PaStream* sac;
+js_pastream_get(JSContext* ctx, JSValueConst this_val, int magic) {
+  PaStream* st;
   JSValue ret = JS_UNDEFINED;
 
-  if(!(sac = static_cast<PaStream*>(JS_GetOpaque2(ctx, this_val, js_pastream_class_id))))
+  if(!(st = JS_GetOpaque2(ctx, this_val, js_pastream_class_id)))
     return JS_EXCEPTION;
 
   switch(magic) {}
@@ -55,12 +58,13 @@ js_pastream_get(JSStream* ctx, JSValueConst this_val, int magic) {
 }
 
 static JSValue
-js_pastream_set(JSStream* ctx, JSValueConst this_val, JSValueConst value, int magic) {
-  PaStream* sac;
+js_pastream_set(JSContext* ctx, JSValueConst this_val, JSValueConst value, int magic) {
+  PaStream* st;
   JSValue ret = JS_UNDEFINED;
 
-  if(!(sac = static_cast<PaStream*>(JS_GetOpaque2(ctx, this_val, js_pastream_class_id))))
+  if(!(st = JS_GetOpaque2(ctx, this_val, js_pastream_class_id)))
     return JS_EXCEPTION;
+
 
   switch(magic) {}
 
@@ -69,11 +73,10 @@ js_pastream_set(JSStream* ctx, JSValueConst this_val, JSValueConst value, int ma
 
 static void
 js_pastream_finalizer(JSRuntime* rt, JSValue val) {
-  PaStream* sac;
+  PaStream* st;
 
-  if((sac = static_cast<PaStream*>(JS_GetOpaque(val, js_pastream_class_id)))) {
-    sac->~PaStream();
-    js_free_rt(rt, sac);
+  if((st = JS_GetOpaque(val, js_pastream_class_id))) {
+    js_free_rt(rt, st);
   }
 }
 
@@ -87,7 +90,7 @@ static const JSCFunctionListEntry js_pastream_funcs[] = {
 };
 
 int
-js_portaudio_init(JSStream* ctx, JSModuleDef* m) {
+js_portaudio_init(JSContext* ctx, JSModuleDef* m) {
   JS_NewClassID(&js_pastream_class_id);
   JS_NewClass(JS_GetRuntime(ctx), js_pastream_class_id, &js_pastream_class);
 
@@ -98,9 +101,6 @@ js_portaudio_init(JSStream* ctx, JSModuleDef* m) {
 
   JS_SetClassProto(ctx, js_pastream_class_id, pastream_proto);
 
-  JS_NewClassID(&js_audiodestinationnode_class_id);
-  JS_NewClass(JS_GetRuntime(ctx), js_audiodestinationnode_class_id, &js_audiodestinationnode_class);
-
   if(m) {
     JS_SetModuleExport(ctx, m, "PaStream", pastream_ctor);
   }
@@ -108,13 +108,13 @@ js_portaudio_init(JSStream* ctx, JSModuleDef* m) {
   return 0;
 }
 
-extern "C" VISIBLE void
-js_init_module_portaudio(JSStream* ctx, JSModuleDef* m) {
+VISIBLE void
+js_init_module_portaudio(JSContext* ctx, JSModuleDef* m) {
   JS_AddModuleExport(ctx, m, "PaStream");
 }
 
-extern "C" VISIBLE JSModuleDef*
-js_init_module(JSStream* ctx, const char* module_name) {
+VISIBLE JSModuleDef*
+js_init_module(JSContext* ctx, const char* module_name) {
   JSModuleDef* m;
 
   if((m = JS_NewCModule(ctx, module_name, js_portaudio_init))) {
